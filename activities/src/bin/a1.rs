df02327a9684aec3a2e0d410cb61f5045f17defa
Victor <mrethanlucas@gmail.com>
2023-08-18 18:32:39 +0100

    updates

---
+++ activities/src/bin/a1.rs
@@ -0,0 +1,30 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+fn main() {
+    let first_name = "page";
+    let second_name = "cc";
+    println!("{:?} {:?}", first_name, second_name);
+}
+
+// Alternatively
+
+// fn display_firstname() {
+//     println!("Page");
+// }
+
+// fn display_last_name() {
+//     println!("CC");
+// }
+
+// fn main() {
+//     display_firstname();
+//     display_last_name();
+// }
