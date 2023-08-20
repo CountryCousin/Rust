1b55f54b425110548901224ea569b9c5a38ea117
Victor <mrethanlucas@gmail.com>
2023-08-20 10:22:14 +0100

    day2

--- day/src/main.rs
+++ day/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
-}
+// Day 2 of Rust, I started with Control flow which are "if", "else if" and "else".
+
+// fn main() {
+//     let toms_age = 500;
+
+//     if toms_age > 700 {
+//         if toms_age > 850 {
+//             println!(" {toms_age} ?, Tom must be an alien");
+//         } else {
+//             println!(" {toms_age} ?, Tom must be an vampire");
+//         }
+//     } else if toms_age > 400 {
+//         println!(" {toms_age}?, Tom must be an alien sha");
+//     } else {
+//         println!("{toms_age} ?, Tom is a hmmmmm!");
+//     }
+// }
+
+// There are two types of loops in Rust: the "while loop" and the "infinite loop(uses the "loop"keyword)""
