4acd3fd17c2ce56488013e352e0c8331de66c76f
Victor <mrethanlucas@gmail.com>
2023-09-04 15:52:31 +0100

    day 15

---
+++ day/target/doc/implementors/core/panic/unwind_safe/trait.RefUnwindSafe.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"day":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"enum\" href=\"day/enum.Color.html\" title=\"enum day::Color\">Color</a>",1,["day::Color"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"day/struct.Mail.html\" title=\"struct day::Mail\">Mail</a>",1,["day::Mail"]]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ day/target/doc/source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"day":["",[],["main.rs"]]\
+}');
+createSourceSidebar();
