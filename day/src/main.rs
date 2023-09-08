a67ce7ca7233cfd2c6c928a9abaa9a9b43e0e639
Victor <mrethanlucas@gmail.com>
2023-09-08 10:47:28 +0100

    day 19

--- day/src/main.rs
+++ day/src/main.rs
@@ -1,57 +1,22 @@
-// Day 18 of Rust, I looked Hashmap Data structures, Hashmap is a collection that stores
-// data as key-value pairs.
-// Data is located using the key and the data is the value. hashmap are very fast to retrive
-// data using the key. In hashmaps, Data is stored Randomly contrary to vector where everything comes in same order
-// we use the "mut" keyword because we will have to manually insert the data
+// Day 19 of Rust, I went over Basic Closures which means an
+// anonymoius function. since closures are anonymous, they must be
+// defined within another function
 
-// fn main() {
-//     // use std::collections::HashMap;
-//     // use std::collections::HashMap;
-//     use std::collections::hash_map::HashMap;
-//     let mut people = HashMap::new();
-//     people.insert("Susan", 21);
-//     people.insert("Ed", 13);
-//     people.insert("Will", 14);
-//     people.insert("Cathy", 22);
-//     people.remove("Susan");
-
-//     match people.get("Ed") {
-//         Some(age) => println!("age = {:?}", age),
-//         None => println!("Not found"),
-//     }
-// }
-
-use std::collections::hash_map::HashMap;
-
-#[derive(Debug)]
-struct Contents {
-    content: String,
+// traditional function declaration
+fn subtract(a: i32, b: i32) -> i32 {
+    b - a
 }
-
 fn main() {
-    // we use the "mut" keyword because we will have to manually insert the data into the hashmap
-    let mut lockers = HashMap::new();
-    lockers.insert(
-        1,
-        Contents {
-            content: "polo".to_owned(),
-        },
-    );
-    lockers.insert(
-        2,
-        Contents {
-            content: "gym_shorts".to_owned(),
-        },
-    );
-    lockers.insert(
-        3,
-        Contents {
-            content: "shoes".to_owned(),
-        },
-    );
+    // calling the traditional function
+    let subtract_it = subtract(6, 2);
 
-    // iterating over the map
-    for (locker_id, element) in lockers.iter() {
-        println!("id: {:?}, element: {:?} ", locker_id, element)
-    }
+    // Declaring a closure
+    // let sub = |a: i32, b: i32| -> i32 { a + b }; // This could work but not necessary
+
+    // common method of declaring closures
+    let sub = |a, b| a - b;
+
+    // calling our anonymous function
+    let difference = sub(4, 1);
+    println!("The difference is: {:?}", difference);
 }
